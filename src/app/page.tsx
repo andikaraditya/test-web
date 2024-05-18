8293e2e7ce3582717a40da72a601b018ed5032e5
andika <andikaraditya1@gmail.com>
2024-05-18 19:11:58 +0700

    adjust content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,27 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
-      
+      <h1 className="text-5xl mb-5">Hello World</h1>
+      <div className="flex  gap-7">
+      <Image 
+      src={"/the-golden-compass.jpeg"}
+      width="500"
+      height="500"
+      alt="The golden compass book cover"
+      />
+      <Image 
+      src={"/the-subtle-knife.png"}
+      width="500"
+      height="500"
+      alt="The subtle knife book cover"
+      />
+      <Image 
+      src={"/the-amber-spyglass.jpeg"}
+      width="500"
+      height="500"
+      alt="The amber spyglass book cover"
+      />
+      </div>
     </main>
   );
 }
