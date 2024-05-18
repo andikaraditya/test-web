4523216aa20d7dd3cf864fc970e35b3435422fbe
andika <andikaraditya1@gmail.com>
2024-05-18 19:15:28 +0700

    deployment setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,19 @@ export default function Home() {
       <h1 className="text-5xl mb-5">Hello World</h1>
       <div className="flex  gap-7">
       <Image 
-      src={"/the-golden-compass.jpeg"}
+      src={"./the-golden-compass.jpeg"}
       width="500"
       height="500"
       alt="The golden compass book cover"
       />
       <Image 
-      src={"/the-subtle-knife.png"}
+      src={"./the-subtle-knife.png"}
       width="500"
       height="500"
       alt="The subtle knife book cover"
       />
       <Image 
-      src={"/the-amber-spyglass.jpeg"}
+      src={"./the-amber-spyglass.jpeg"}
       width="500"
       height="500"
       alt="The amber spyglass book cover"
